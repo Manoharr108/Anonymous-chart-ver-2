52b136d83d1398ada3aa2031a307d2016b995062
manoharr108 <manohar2004gr@gmail.com>
2024-05-12 00:18:35 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+const cors = require('cors')
+app.use(cors())
+const data = require('./public/data.json')
+
+const port = 3000
+app.use(express.static("public"))
+app.use(express.json())
+ 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/index.html"))
+})
+
+app.post('/data',(req,res)=>{
+    let result = req.body
+    let file = fs.readFileSync('./public/data.json','utf-8')
+    let parsedData = file ? JSON.parse(file) : []
+    parsedData.push(result)
+    fs.writeFileSync('./public/data.json',JSON.stringify(parsedData,null,2),'utf-8')
+})
+
+
+app.get('/data',(req,res)=>{
+    res.send(data)
+})
+
+
+app.listen(port,()=>{
+    console.log(`app listening on ${port}`)
+})
